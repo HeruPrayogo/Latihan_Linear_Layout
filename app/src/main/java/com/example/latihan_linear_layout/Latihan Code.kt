b49935f880cfbe05a000afd15428cfc04fb74b53
heru prayogo <heruprayogo062@gmail.com>
2023-03-15 19:16:35 +0700

    a

---
+++ app/src/main/java/com/example/latihan_linear_layout/Latihan Code.kt
@@ -0,0 +1,11 @@
+package com.example.latihan_linear_layout
+
+object UpperCase {
+    const val MAX_VALUE = "Heru"
+}
+
+class PascalCase{
+    fun cammelCase(){
+        val camelClass:Int = 90
+    }
+}
